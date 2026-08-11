d72bf2dbb422bebb8614548199a7bfd8beef4d83
FlipThisCrypto <flipthiscrypto@gmail.com>
2026-08-11 19:07:57 -0400

    round 2 iteration 37: expand public barrel with Round 2 logic modules

--- src/engine/index.ts
+++ src/engine/index.ts
@@ -22,6 +22,30 @@ export * from './deckExport';
 export * from './deckPersistence';
 export * from './elements';
 export * from './abilities';
+export * from './turnMachine';
+export * from './actions';
+export * from './matchSession';
+export * from './legalMoves';
+export * from './effectIr';
+export * from './effectInterpreter';
+export * from './stack';
+export * from './matchObservability';
+export * from './replay';
+export * from './idempotency';
+export * from './triggers';
+export * from './costPayment';
+export * from './continuousEffects';
+export * from './matchConfig';
+export * from './priority';
+export * from './echoQueue';
+export * from './assertInvariants';
+export * from './replacementEffects';
+export * from './aiPolicy';
+export * from './serialization';
+export * from './mulliganStrategy';
+export * from './combatPlanner';
+export * from './healthCheck';
+export * from './schemaVersion';
 export {
   createGameState,
   createCardInstance,
